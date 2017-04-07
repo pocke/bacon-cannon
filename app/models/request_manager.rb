a182f8c11b30bb0d32cc331d6d7d107eafef9ee1
Masataka Kuwabara <kuwabara@pocke.me>
2017-04-07 23:36:10 +0900

    Introduce bootstrap

--- app/models/request_manager.rb
+++ app/models/request_manager.rb
@@ -1,11 +1,11 @@
 module RequestManager
   Parsers = {
     'ripper_19' => ENV.fetch('RIPPER19_URL'){'https://ripper-api-for-ruby19.herokuapp.com/'},
-    'ripper_20' => ENV.fetch('RIPPER19_URL'){'https://ripper-api-for-ruby20.herokuapp.com/'},
-    'ripper_21' => ENV.fetch('RIPPER19_URL'){'https://ripper-api-for-ruby21.herokuapp.com/'},
-    'ripper_22' => ENV.fetch('RIPPER19_URL'){'https://ripper-api-for-ruby22.herokuapp.com/'},
-    'ripper_23' => ENV.fetch('RIPPER19_URL'){'https://ripper-api-for-ruby23.herokuapp.com/'},
-    'ripper_24' => ENV.fetch('RIPPER19_URL'){'https://ripper-api-for-ruby24.herokuapp.com/'},
+    'ripper_20' => ENV.fetch('RIPPER20_URL'){'https://ripper-api-for-ruby20.herokuapp.com/'},
+    'ripper_21' => ENV.fetch('RIPPER21_URL'){'https://ripper-api-for-ruby21.herokuapp.com/'},
+    'ripper_22' => ENV.fetch('RIPPER22_URL'){'https://ripper-api-for-ruby22.herokuapp.com/'},
+    'ripper_23' => ENV.fetch('RIPPER23_URL'){'https://ripper-api-for-ruby23.herokuapp.com/'},
+    'ripper_24' => ENV.fetch('RIPPER24_URL'){'https://ripper-api-for-ruby24.herokuapp.com/'},
   }.freeze
 
   # @param code [String] A Ruby code
