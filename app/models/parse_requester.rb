65d2f4851d85df023d2c254675d1da8343dd9938
Masataka Kuwabara <kuwabara@pocke.me>
2017-04-09 12:33:57 +0900

    Refactor

--- app/models/parse_requester.rb
+++ app/models/parse_requester.rb
@@ -1,5 +1,15 @@
 module ParseRequester
-  def self.request(code, url)
+  API_URLS = {
+    'ripper_24' => ENV.fetch('RIPPER24_URL'){'https://ripper-api-for-ruby24.herokuapp.com/'},
+    'ripper_23' => ENV.fetch('RIPPER23_URL'){'https://ripper-api-for-ruby23.herokuapp.com/'},
+    'ripper_22' => ENV.fetch('RIPPER22_URL'){'https://ripper-api-for-ruby22.herokuapp.com/'},
+    'ripper_21' => ENV.fetch('RIPPER21_URL'){'https://ripper-api-for-ruby21.herokuapp.com/'},
+    'ripper_20' => ENV.fetch('RIPPER20_URL'){'https://ripper-api-for-ruby20.herokuapp.com/'},
+    'ripper_19' => ENV.fetch('RIPPER19_URL'){'https://ripper-api-for-ruby19.herokuapp.com/'},
+  }.freeze
+
+  def self.request(code, parser_name)
+    url = API_URLS[parser_name]
     conn = Faraday.new(url: url) do |f|
       f.adapter Faraday.default_adapter
       f.response :raise_error
--- app/models/request_manager.rb
+++ app/models/request_manager.rb
@@ -1,26 +1,26 @@
 module RequestManager
-  Parsers = {
-    'ripper_24' => ENV.fetch('RIPPER24_URL'){'https://ripper-api-for-ruby24.herokuapp.com/'},
-    'ripper_23' => ENV.fetch('RIPPER23_URL'){'https://ripper-api-for-ruby23.herokuapp.com/'},
-    'ripper_22' => ENV.fetch('RIPPER22_URL'){'https://ripper-api-for-ruby22.herokuapp.com/'},
-    'ripper_21' => ENV.fetch('RIPPER21_URL'){'https://ripper-api-for-ruby21.herokuapp.com/'},
-    'ripper_20' => ENV.fetch('RIPPER20_URL'){'https://ripper-api-for-ruby20.herokuapp.com/'},
-    'ripper_19' => ENV.fetch('RIPPER19_URL'){'https://ripper-api-for-ruby19.herokuapp.com/'},
+  Parsers = %w[
+    ripper_24
+    ripper_23
+    ripper_22
+    ripper_21
+    ripper_20
+    ripper_19
 
-    'parser_24' => nil,
-    'parser_23' => nil,
-    'parser_22' => nil,
-    'parser_21' => nil,
-    'parser_20' => nil,
-    'parser_19' => nil,
-    'parser_18' => nil,
-  }.freeze
+    parser_24
+    parser_23
+    parser_22
+    parser_21
+    parser_20
+    parser_19
+    parser_18
+  ].freeze
 
   # @param code [String] A Ruby code
   # @param targets [Array<String>] parser names
   # @return [Array<String>]
   def self.request(code, targets)
-    targets &= Parsers.keys
+    targets &= Parsers
     targets
       .map{|name| command(code, name)}
       .map(&:start)
@@ -29,17 +29,10 @@ module RequestManager
   end
 
   def self.command(code, parser_name)
-    # XXX: Hack for autoload
-    ParseRequester
-    ParserWithParser
+    klass = parser_name.start_with?('ripper_') ? ParseRequester : ParserWithParser
 
     Expeditor::Command.new do
-      if parser_name.start_with?('ripper_')
-        url = Parsers[parser_name]
-        ParseRequester.request(code, url)
-      else # parser
-        ParserWithParser.request(code, parser_name)
-      end
+      klass.request(code, parser_name)
     end.set_fallback do |e|
       Rails.logger.warn e
       nil
