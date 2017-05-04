9e862776f214a75e5b69493be99a36ac74cf35bd
Masataka Kuwabara <kuwabara@pocke.me>
2017-05-04 18:51:34 +0900

    Add test

--- test/system/parse_test.rb
+++ test/system/parse_test.rb
@@ -27,4 +27,34 @@ class ParseTest < ApplicationSystemTestCase
       assert page.text.include?('unexpected token')
     end
   end
+
+  def test_parse_and_create_a_parmlink
+    visit root_path
+
+    fill_in 'ruby_code', with: "puts 'Hello, world!'"
+    uncheck 'ripper_24'
+    click_on 'Parse'
+
+    pre = page.find('pre')
+    assert_equal pre.text, <<~END.chomp
+      s(:send, nil, :puts, s(:str, "Hello, world!"))
+    END
+
+    prev_permlink_count = Parmlink.count
+    click_on 'Parmlink'
+    sleep 2
+
+    assert_equal prev_permlink_count, Parmlink.count - 1
+
+    visit root_path
+    assert find('[name=ruby_code]').text == ''
+
+    link = Parmlink.last
+    visit parmlink_path(link.uuid)
+    assert find('[name=ruby_code]').text == "puts 'Hello, world!'"
+    pre = page.find('pre')
+    assert_equal pre.text, <<~END.chomp
+      s(:send, nil, :puts, s(:str, "Hello, world!"))
+    END
+  end
 end
